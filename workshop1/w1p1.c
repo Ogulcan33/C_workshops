e6c0a7eb390b289239e32433f9502a60d69bdb89
Ogulcan33 <89296567+Ogulcan33@users.noreply.github.com>
2025-07-11 13:25:57 +0300

    Add files via upload

---
+++ workshop1/w1p1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    printf("Workshop 1 Part-1\n=================\n\n");
+    printf("I'm displaying this using the 'printf' stdio\n");
+    printf("(standard input output) library function!\n\n");
+    printf("Dear Teacher,\n\n");
+    printf(" I promise I will work hard from this day onward.\n");
+    printf(" I acknowledge that practice is extremely important,\n");
+    printf(" so I will do all workshops, quizzes, and assingments.\n\n");
+    printf("Sincerely,\n\n");
+    printf("Ogulcan Yazicioglu\n");
+    printf("Student ID# 123456789");
+}
